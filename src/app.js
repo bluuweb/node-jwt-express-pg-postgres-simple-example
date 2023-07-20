64cedc31ae331d9ce230518eba1c3b62795a1683
bluuweb <contacto@bluuweb.cl>
2023-07-20 11:55:01 -0400

    add string url postgres

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
+import cors from "cors";
 import express from "express";
+import bookRouter from "./book/book.route.js";
 import seedRouter from "./seed/seed.route.js";
 import userRouter from "./user/user.route.js";
-import bookRouter from "./book/book.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/seed", seedRouter);
 app.use("/api/v1/user", userRouter);
