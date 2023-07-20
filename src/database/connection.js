24f627fb0a51a2a51daaf607492e047cfe7bc7dc
bluuweb <contacto@bluuweb.cl>
2023-07-20 19:40:34 -0400

    change yarn

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,6 +4,23 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
-export const pool = new Pool({
-  allowExitOnIdle: true,
-});
+const connectionString = process.env.PG_URL;
+
+export const pool = connectionString
+  ? new Pool({
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+      allowExitOnIdle: true,
+    })
+  : new Pool({
+      allowExitOnIdle: true,
+    });
+
+try {
+  await pool.query("SELECT NOW()");
+  console.log("Database connected");
+} catch (error) {
+  console.log(error);
+}
